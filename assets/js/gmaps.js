635283994709d035d1a2624b1a661914aa66b3d0
Nicola Lamacchia <nicola.lamacchia@gmail.com>
2014-09-05 17:47:54 +0200

    Update gmaps.js: use OpenStreetMap

--- assets/js/gmaps.js
+++ assets/js/gmaps.js
@@ -2,10 +2,20 @@ var parkingLatLon=new google.maps.LatLng(41.312544,16.281694);
 var gosLatLon=new google.maps.LatLng(41.312557,16.278122);
 function initialize()
 {
+    var mapTypeIds = [];
+    for(var type in google.maps.MapTypeId) {
+        mapTypeIds.push(google.maps.MapTypeId[type]);
+    }
+    mapTypeIds.push("OSM");
+
     var mapProp = {
         center: gosLatLon,
-        zoom:14,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        zoom: 15,
+        // mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: "OSM",
+        mapTypeControlOptions: {
+            mapTypeIds: mapTypeIds
+        }
     };
 
     var map = new google.maps.Map(document.getElementById("map-canvas"),mapProp);
@@ -26,12 +36,20 @@ function initialize()
     markerGOS.setMap(map);
 
     google.maps.event.addListener(markerParking,'click',function() {
-        map.setZoom(16);
+        map.setZoom(17);
         map.setCenter(markerParking.getPosition());
     });
     google.maps.event.addListener(markerGOS,'click',function() {
-        map.setZoom(16);
+        map.setZoom(17);
         map.setCenter(markerGOS.getPosition());
     });
+    map.mapTypes.set("OSM", new google.maps.ImageMapType({
+        getTileUrl: function(coord, zoom) {
+            return "http://tile.openstreetmap.org/" + zoom + "/" + coord.x + "/" + coord.y + ".png";
+        },
+        tileSize: new google.maps.Size(256, 256),
+        name: "OpenStreetMap",
+        maxZoom: 18
+    }));
 }
 google.maps.event.addDomListener(window, 'load', initialize);
